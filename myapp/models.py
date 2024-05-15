78cbb0ef699761edbff4c550a5f9816fa496e502
ShahadAlkamli <85537889+ShahadAlkamli@users.noreply.github.com>
2024-05-16 00:10:56 +0300

    Delete myapp directory

--- myapp/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Transaction(models.Model):
-    # Define model fields here
-    text = models.CharField(max_length=255)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
